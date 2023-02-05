e0612e63dddaa1bcc1a6d865f4a89bc1e81bcf80
DouglasCBS <douglascesar12@gmail.com>
2023-02-05 13:01:08 -0300

    validate matching passwords

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-const btn = document.querySelector('button[type="submit"]')
+const btn = document.querySelector('button[type="submit"]');
+const password = document.querySelector("#password");
+const confPassword = document.querySelector("#confirm-password");
+const message = document.querySelector(".message");
 
-btn.addEventListener("click", () => console.log(this))
+btn.addEventListener("click", checkPassword);
+
+function checkPassword() {
+    if ((password.value.length !== 0 && confPassword.value.length !== 0) && (confPassword.value !== password.value)) {
+        errorPass()
+        return false
+    }
+    else if (confPassword.value === password.value) {
+        password.classList.remove("error");
+        confPassword.classList.remove("error");
+        message.textContent = "";
+        return true
+    }
+};
+
+function errorPass() {
+    confPassword.classList.add("error");
+    password.classList.add("error");
+    message.textContent = "* passwords do not match.";
+    message.setAttribute("style", "color: red");
+};
